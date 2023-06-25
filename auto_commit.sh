6a70e924e67710136111a9196faf3e467b21aa72
Francisco Castillo <kiko@arize.com>
2023-06-25 15:53:14 -0700

    Update autocommit script

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,7 +1,12 @@
-for i in {1..50}; do
-    printf "\n\n" >> README.md
-    echo "Commit $i" >> README.md
-    git add README.md
-    git commit -m "Add new commit $i"
+for minor in {0..9}; do
+    for patch  in {0..5}; do
+        version="0.${minor}.${patch}"
+      echo "__version__ = \"${version}\"" > my_helper_package/src/ilsadvbisnvavojebaij/__init__.py
+      printf "\n\n" >> README.md
+      echo "New commit: Bump to version ${version}" >> README.md
+      git add README.md
+      git add my_helper_package/src/ilsadvbisnvavojebaij/__init__.py
+      git commit -m "Bump to version ${version}"
+    done
 done
 
