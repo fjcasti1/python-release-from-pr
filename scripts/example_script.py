b5aaa43f9f7df8988a9b5d4eea4eb48d514ce8e8
Francisco Castillo <kiko@arize.com>
2023-06-25 13:25:12 -0700

    Add all files

---
+++ scripts/example_script.py
@@ -0,0 +1,27 @@
+import emoji
+import numpy as np
+from my_helper_package.math import scalars as scalar_math
+from my_helper_package.math import vectors as vector_math
+
+
+def main():
+    info_em = emoji.emojize(":information:")
+    s_1 = 3
+    s_2 = 7
+    print(f"{info_em} Scalar math: {s_1} + {s_2} = {scalar_math.add(s_1, s_2)}")
+    print(f"{info_em} Scalar math: {s_1} - {s_2} = {scalar_math.subtract(s_1, s_2)}")
+    print(f"{info_em} Scalar math: {s_1} * {s_2} = {scalar_math.multiply(s_1, s_2)}")
+    print(f"{info_em} Scalar math: {s_1} / {s_2} = {scalar_math.divide(s_1, s_2)}")
+    print(" ")
+    v_1 = np.array([1, 2, 3])
+    v_2 = np.array([4, 5, -3])
+    print(f"{info_em} Vector math: {v_1} + {v_2} = {vector_math.add(v_1, v_2)}")
+    print(f"{info_em} Vector math: {v_1} - {v_2} = {vector_math.subtract(v_1, v_2)}")
+    print(f"{info_em} Vector math: {v_1} * {v_2} = {vector_math.multiply(v_1, v_2)}")
+    print(f"{info_em} Vector math: {v_1} / {v_2} = {vector_math.divide(v_1, v_2)}")
+    print(f"{info_em} Vector math: {v_1} . {v_2} = {vector_math.dot_prod(v_1, v_2)}")
+    return
+
+
+if __name__ == "__main__":
+    main()
