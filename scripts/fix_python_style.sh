dd1e040501b9ccd6974c224ce45b11225ae082b3
Francisco Castillo <kiko@arize.com>
2023-06-25 16:26:11 -0700

    Fix style python script

--- scripts/fix_python_style.sh
+++ scripts/fix_python_style.sh
@@ -30,7 +30,7 @@ isort --profile black \
       --extend-skip-glob="*_pb2_grpc.py" \
       --line-length ${line_length} \
       --thirdparty "arize" \
-      python/ sdk/python/
+      ./
 
 # Check black formatting
 echo " "
@@ -38,7 +38,7 @@ echo "Fixing format with black"
 echo "========================"
 black --line-length ${line_length} \
       --extend-exclude=".*_pb2.py|.*_pb2_grpc.py|python\/umap_learn\/.*|python\/tutorials\/.*|python\/data_generation\/.*|.*.ipynb"\
-      python/ sdk/python/
+      ./
 
 # Run flake8
 echo " "
@@ -47,4 +47,4 @@ echo "===================="
 flake8 --max-line-length $((line_length+10)) \
        --extend-exclude=*_pb2.py,*_pb2_grpc.py,python/umap_learn/*,python/tutorials/* \
        --ignore=E203,E266,W503 \
-       python/ sdk/python/
+       ./
