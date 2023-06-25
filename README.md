1e5320ae84a09160d00dcd87da6b758098ef5098
Francisco Castillo <kiko@arize.com>
2023-06-25 13:38:55 -0700

    Reset README

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
 # python-release-from-pr
-
-
-Commit 1
-
-
-Commit 2
-
-
-Commit 3
-
-
-Commit 4
-
-
-Commit 5
--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,7 @@
 for i in {1..50}; do
+    printf "\n\n" >> README.md
     echo "Commit $i" >> README.md
     git add README.md
-    git commit -m "Add commit $i"
+    git commit -m "Add new commit $i"
 done
 
